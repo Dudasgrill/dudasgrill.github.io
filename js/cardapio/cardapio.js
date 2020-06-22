81f9b7416e3804c451312e754b929f2e0dc39b91
Dudasgrill <67119385+Dudasgrill@users.noreply.github.com>
2020-06-21 21:42:19 -0300

    Add files via upload

--- js/cardapio/cardapio.js
+++ js/cardapio/cardapio.js
@@ -6,11 +6,13 @@ var myFunction = function() {
         name = this.children[0].children[0].textContent.replace(/\s{2,}/g, ' ');
         descr = this.children[0].children[1].textContent.replace(/\s{2,}/g, ' ');
         price = this.children[1].children[0].textContent;
+        rmfouthletters = name.trim();
         rmfouthletters = name.split('');
-        rmfouthletters = rmfouthletters.splice(4, rmfouthletters.length).join('');
-        console.log(rmfouthletters);
-        console.log(descr);
-        console.log(price);
+        rmfouthletters = rmfouthletters.splice(1, rmfouthletters.length).join('');
+        rmfouthletters = rmfouthletters.replace('-','').trim();
+        //console.log(rmfouthletters);
+        //console.log(descr);
+        //console.log(price);
         window.open("https://api.whatsapp.com/send?phone=5521964391013&text="+"Oi, quero pedir este item do cardápio "+'"'+rmfouthletters+'" preço: '+price);
     }
 };
